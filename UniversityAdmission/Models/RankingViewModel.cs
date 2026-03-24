dff5d43d593a05d9032edb37ff3644f1c1c2a6d6
LiliaEncheva <encevalilia@gmail.com>
2026-03-24 12:21:00 +0200

    changes

---
+++ UniversityAdmission/Models/RankingViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using UniversityAdmission.Models.Entities;
+using UniversityAdmission.Models.Enums;
+
+namespace UniversityAdmission.Models
+{
+    public class RankingViewModel
+    {
+        public List<Application> Applications { get; set; }
+
+        // Filters
+        public int? SpecialityId { get; set; }
+        public int? PreferenceOrder { get; set; }
+        public ApplicationStatus? Status { get; set; }
+
+        // Dropdowns
+        public List<SelectListItem> Specialities { get; set; }
+
+        // Paging
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPages { get; set; }
+    }
+}
